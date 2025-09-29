3516a272d338e7eeabe04e76a8cbf00885167b91
alex <132571895+belcouact@users.noreply.github.com>
2025-09-29 16:16:38 +0800

    delete

--- frontend/src/store/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineStore } from 'pinia'
-
-export const useAuthStore = defineStore('auth', {
-  state: () => ({ token: '' as string, user: null as any }),
-  actions: {
-    login(token: string, user: any) { this.token = token; this.user = user },
-    logout() { this.token = ''; this.user = null }
-  }
-})
